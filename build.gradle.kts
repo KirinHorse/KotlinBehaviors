302b35c6f51a484ba57e145b7b159bc22b9e30dd
KirinHorse <xinqikj@hotmail.com>
2023-08-10 23:08:07 +0800

    update

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,9 +1,10 @@
 plugins {
     kotlin("jvm") version "1.8.21"
     `java-library`
+    `maven-publish`
 }
 
-group = "org.kirinhorse"
+group = "io.github.kirinhorse"
 version = "0.0.1"
 
 repositories {
@@ -13,7 +14,7 @@ repositories {
 dependencies {
     implementation(kotlin("reflect"))
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.7.3")
-    
+
     testImplementation(kotlin("test"))
 }
 
